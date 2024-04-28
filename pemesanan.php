4e372e130e8e99d4ff769594333b22b4207a7090
suardinatawayan <suardinatawayan@gmail.com>
2024-04-28 20:42:59 +0700

    index.php selesai

---
+++ pemesanan.php
@@ -0,0 +1,23 @@
+<?php include "layouts/header.php"  ?>
+
+
+        <div  class="row mt-3">
+            <div class="col-12 col-md-9">
+                <h2>Form Pemesanan Paket Wisata</h2>
+
+            </div>
+            <?php include "layouts/sidebar.php"?>
+
+        </div>
+
+        <!-- Footer -->
+      <?php include "layouts/footer.php"; ?>
+
+    </div>
+
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
